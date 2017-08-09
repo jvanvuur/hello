a1f0810e8b3c5740474b9a52a080bb4062191f85
Mark Hopson <mark.hopson@wishabi.com>
2017-08-09 17:58:55 -0400

    ...

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,10 +1,10 @@
 cd ./deploy
 
 # ENV Variables that are used the the platform-service-deploy.sh script
-export ECS_CLUSTER=$ECS_CLUSTER_STG
-export ECS_SERVICE=$ECS_SERVICE_STG
-export SERVICE_CONTAINER_NAME=$ECS_SERVICE_STG
-export CONTAINER_IMAGE=$ECR_REPOSITORY:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
+export ECS_CLUSTER=services-ecs-stg
+export ECS_SERVICE=hello
+export SERVICE_CONTAINER_NAME=hello
+export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/hello:staging-3
 
 #download latest deploy script
 aws s3 cp "s3://flipp-platform-"$WISHABI_ENVIRONMENT"/deploy/platform-service-deploy.sh" platform-service-deploy.sh
