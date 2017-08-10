213e54ad9e2814fc3abc9c189e904ade623854fb
Mark Hopson <mark.hopson@wishabi.com>
2017-08-10 10:22:31 -0400

    ...

--- app.js
+++ app.js
@@ -3,18 +3,6 @@ const express = require('express');
 const app = express();
 const router = express.Router()
 
-var StatsD = require('node-dogstatsd').StatsD;
-c = new StatsD('localhost', 8125);
-
-var dd_options = {
-  'datadog': c,
-  'response_code': true,
-  'tags': ['app:mhopson_app'] 
-}
-
-var connect_datadog = require('connect-datadog')(dd_options);
-
-app.use(connect_datadog);
 app.use(router);
 
 app.get('/', (req, res) => {
