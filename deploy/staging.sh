9f8239a0e456697d543be7644b9963a734929418
Mark Hopson <mark.hopson@wishabi.com>
2017-08-10 10:06:16 -0400

    ...

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -7,6 +7,7 @@ export SERVICE_CONTAINER_NAME=hello
 export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/hello:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
 export SERVICE_TOKEN=42816376-62C7-4F60-BF66-CB525401C500
 export AWS_REGION=us-east-1
+export WISHABI_ENVIRONMENT=development
 
 #download latest deploy script
 aws s3 cp "s3://flipp-platform-development/deploy/platform-service-deploy.sh" platform-service-deploy.sh
