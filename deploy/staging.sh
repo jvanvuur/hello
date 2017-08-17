47ba8cde53b80f2f4569613eab114693196cd124
Mark Hopson <mark.hopson@wishabi.com>
2017-08-17 12:28:24 -0400

    ...

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -2,7 +2,6 @@ cd ./deploy
 
 # ENV Variables that are used the the platform-service-deploy.sh script
 export ECS_CLUSTER=$ECS_CLUSTER_STG
-export ECS_SERVICE=$ECS_SERVICE_STG
 export SERVICE_CONTAINER_NAME=$ECS_SERVICE_STG
 export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/${ECS_SERVICE}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
 
