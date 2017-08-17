b64f841af96f35a22fcd3a2580349d0a697d5638
Mark Hopson <mark.hopson@wishabi.com>
2017-08-17 12:35:32 -0400

    ...

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -2,8 +2,9 @@ cd ./deploy
 
 # ENV Variables that are used the the platform-service-deploy.sh script
 export ECS_CLUSTER=$ECS_CLUSTER_STG
+export ECS_SERVICE=$ECS_SERVICE_STG
 export SERVICE_CONTAINER_NAME=$ECS_SERVICE_STG
-export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/${ECS_SERVICE}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
+export CONTAINER_IMAGE=280159221453.dkr.ecr.us-east-1.amazonaws.com/${ECS_SERVICE_NAME}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
 
 #download latest deploy script
 aws s3 cp "s3://flipp-platform-development/deploy/platform-service-deploy.sh" platform-service-deploy.sh
